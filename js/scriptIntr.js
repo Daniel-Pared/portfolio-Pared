d4050e7fcc27df8bb1ae4e2ffb51f6f2637f00aa
Daniel Pared <dan_pared@hotmail.com>
2022-01-17 15:00:59 -0300

    Changes from merging branch script.

--- js/scriptIntr.js
+++ js/scriptIntr.js
@@ -65,60 +65,6 @@ function newURL() {
 //Use of Enter key to go to home page.
 
 window.addEventListener("keydown", (event) => {
-<<<<<<< HEAD
-    if (event.key === 'Enter') {
-        newURL();
-
-        if (file.localeCompare(fileEs) !== 0) {
-            window.location.href = `${route}/pages/home.html`;
-        }
-        else {
-            window.location.href = `${route}/home.html`;
-        };
-    };
-    
-});
-
-
-//Change the language website.
-
-langSelect.addEventListener('click', changeLang);
-
-function changeLang() {
-    newURL();
-
-    if (file.localeCompare(fileEs) !== 0) {
-        window.location.href = `${route}/${fileEs}/${page}`;
-    }
-    else {
-        varLength = file.length + 1;
-        route = route.substring(0, route.length - varLength);
-        window.location.href = `${route}/${page}`;
-    }
-}
-
-//Change background video [---- IN CONSTRUCTION -----].
-
-let screenWidth = window.screen.width;
-console.log(screenWidth);
-
-window.addEventListener("resize", getScreenReSize);
-
-function getScreenReSize () {
-    let x = window.screen.width;
-    console.log(x);
-};
-
-//let video = document.getElementById("bg-video");
-//var videoSource = video.getElementsByTagName("source");
-//var video1 = videoSource[1];
-//console.log(video1);
-
-//if (screenW >= 576) {
-//    video1.onplay();
- //   console.log(videoSource);
-//}
-=======
 
     if (event.key === 'Enter') {
         if (langSelect.checked == true) {
@@ -129,4 +75,3 @@ function getScreenReSize () {
         }
     } 
 });
->>>>>>> script
