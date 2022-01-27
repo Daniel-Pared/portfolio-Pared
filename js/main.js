28c591837a286c99b52c823810466e9f21e64d8e
Daniel Pared <dan_pared@hotmail.com>
2022-01-27 09:36:27 -0300

    Delete files. Changes in the .scss files.

--- js/main.js
+++ js/main.js
@@ -1,64 +1,77 @@
 
-
-const filePage = "pages"
-const fileEs = "es";
-const index = "index.html";
 let langSelect = document.getElementById("lang-select");
-let route;
-let page;
-let file;
-let varLength;
 
-function asingValues () {
+//Change background video.
+
+function videoSource () {
+    let size = window.screen.width;
+    let vid = document.getElementById("bg-video");
+
+    if(size >= 500)  {
+        if (size >= 1024){
+            vid.setAttribute("src", "./media/video/bg_video_lg.mp4");
+        }
+        else {
+            vid.setAttribute("src", "./media/video/bg_video_md.mp4");
+        }    
+    }
+    else {
+        vid.setAttribute("src", "./media/video/bg_video_xs.mp4")
+    }
+}
+
+window.addEventListener("resize", videoSource);
+
+//Change language.
+
+function changeLanguage () {
+    let htmlLang = document.documentElement;
+    let enterLink = document.getElementById('enter-btn');
+
+    if (langSelect.checked == true) {
+        htmlLang.setAttribute("lang", "es");
+        document.getElementById('subtittle').innerText = "DESARROLLADOR FRONT-END";
+        enterLink.innerText = "ENTRAR";
+        enterLink.setAttribute("href", "./es/home.html");
+        
+    }
+    else {
+        htmlLang.setAttribute("lang", "en");
+        document.getElementById('subtittle').innerText = "FRONT-END DEVELOPER";
+        enterLink.innerText = "ENTER";
+        enterLink.setAttribute("href", "./pages/home.html");
+    }
+}
+
+langSelect.addEventListener('click', changeLanguage);
+
+//Used when the page is open the first time.
+
+function watch () {
+    videoSource();
+}
+
+watch();
+
+function newURL() {
     route = window.document.URL;
     page = route.split("/").pop();
     varLength = page.length + 1;
     file = route.substring(0, route.length - varLength).split("/").pop();
     route = route.substring(0, route.length - varLength);
-};
-
-function newURL() {
-    if (file.localeCompare(filePage) == 0 || file.localeCompare(fileEs) == 0) {
-        varLength = file.length + 1;
-        route = route.substring(0, route.length - varLength);    
-    }
-};
+}
 
 
-//Change the language website.
+//Use of Enter key to go to home page.
 
-langSelect.addEventListener('click', changeLang);
+window.addEventListener("keydown", (event) => {
 
-function changeLang() {
-    asingValues();
-    newURL();
-
-    if  (langSelect.checked === true) {
-        window.location.href = `${route}/${fileEs}/${page}`;
-    }
-    else if (langSelect.checked == false) {
-        window.location.href = `${route}/${filePage}/${page}`;
-    }
-};
-
-
-// Menu activation.
-
-let menuButton = document.getElementById("menu-icon");
-let hideMenu = document.getElementById("down-header");
-let active = true;
-
-
-menuButton.addEventListener('click', activeMenu);
-
-function activeMenu() {
-    if (active == true) {
-        hideMenu.style.display="block";
-        active = false;
-    }
-    else {
-        hideMenu.style.display="none";
-        active = true;
-    }
-    return active;        
-};
+    if (event.key === 'Enter') {
+        if (langSelect.checked == true) {
+            window.location.href = "./es/home.html";
+        }
+        else {
+            window.location.href = "./pages/home.html";
+        }
+    } 
+});
